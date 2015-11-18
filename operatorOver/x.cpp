5db8cc42938866b0b4707892557f807730ddfbd6
zl <zl@santi.com>
2015-11-18 21:37:12 +0800

    ci

--- operatorOver/x.cpp
+++ operatorOver/x.cpp
@@ -1,40 +1,80 @@
-/**
- * @file     x.cpp
- *           
- *
- * @author   lili  <lilijreey@gmail.com>
- * @date     2015年11月14日 12时08分02秒
- *
- */
-#include <stdlib.h>
+
+#include <iostream>
+#include <stdio.h>
 class Integer {
  public:
   int i;
   Integer* This() { return this; }
  public:
-  explicit Integer(int ll = 0) : i(ll) {}
+  Integer(int ll = 0) : i(ll) {
+    printf("Integer xx\n");
+  }
+
+  Integer(const Integer & o) {
+    this->i = o.i;
+    printf("Integer(const Integer)\n");
+  }
+
+  void show() const {
+    printf("show() :%d %p\n", i, this);
+  }
 };
 
 //+
 
-Integer& operator+(const Integer & o) {
+const Integer& operator+(const Integer & o) {
+  std::cout << __func__ << '\n';
   return o;
 }
 
 Integer operator-(const Integer & o) {
+  std::cout << __func__ << '\n';
   return -o.i;
+
+  //return operator-(o.i);
+  //return x;
+  //return Integer(-o.i);
+  return -o.i;
+}
+
+Integer operator++(Integer &o) {
+  ++o.i;
+  return Integer(o.i);
+}
+
+Integer operator++(Integer &o, int) {
+  int t = o.i;
+  ++o.i;
+  return t;
+}
+
+int inc(int f) {
+  return inc(f++);
 }
 
 int main(int argc, char *argv[])
 {
-  int _i =4;
-  +_i;
-  -_i;
+  //int _i =4;
+  //int x = ++_i;//5
+  //int y = _i++;// 6
+//  +_i;
+  //int x = -_i;
+
 
   Integer i(3);
-  +i;
-  -i;
+  //Integer(3);
 
+  operator+(i); //+i
+  +i; // operator+(i);
+  printf("+i\n");
+  (-i).show();
+
+  i.show();
+  (++i).show();
+  int xxxx=0;
+  (i++).show();
+  i.show();
   
+
   return 0;
 }
